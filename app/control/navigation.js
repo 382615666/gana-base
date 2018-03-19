2eb1b58b6d668327f9d42ecf0fe33843dada7ec0
王湘赣 <wangxianggan@hnjing.com>
2018-03-19 19:15:23 +0800

    a

--- app/control/navigation.js
+++ app/control/navigation.js
@@ -4,7 +4,16 @@ const getNavigation = async(ctx, next) => {
 }
 
 const addNavigation = async(ctx, next) => {
-    const a = await service.addNavigation(ctx.request.body)
+    const result = await service.addNavigation(ctx.request.body)
+    if (result) {
+        ctx.body = {
+            data: result
+        }
+    } else {
+        ctx.body = {
+            err: '新增失败'
+        }
+    }
 }
 
 export default {
--- app/service/navigation.js
+++ app/service/navigation.js
@@ -1,8 +1,12 @@
 import Navigation from '../model/navigation'
 import util from '../../util/util'
 
+const getNavigation = async (params) => {
+
+}
 
 const addNavigation = async (params) => {
+    console.log(new Navigation(params).save())
     return await util.toPromise(callback => {
         new Navigation(params).save(callback)
     })
